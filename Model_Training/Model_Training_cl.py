ba158f1fe59fc3e5604d72fcfac6cf3ffd66b9ab
zxy_ark <1241094178@qq.com>
2025-06-23 20:21:24 +0800

    解冻final_fc

--- Model_Training/Model_Training_cl.py
+++ Model_Training/Model_Training_cl.py
@@ -42,15 +42,35 @@ Test_Data = Build_Dataset(Test_File, 'DBP')
 
 if __name__ == '__main__':
     Seed(6)
-    model = CF_Basic_s.Resnet34_1D()
-    Settings = {'BP_optimizer': 'torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999), weight_decay=0)',
-                'trainer': 'Model_Trainer(model,torch.nn.MSELoss(),BP_optimizer,device,Settings,batch_size=32,num_epochs=40,save_states=True,save_final=True)'}
-
     torch.cuda.empty_cache()
     device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
     print(torch.cuda.get_device_name(0))
+
+    # # 定义模型
+    # model = CF_Basic_s.Resnet34_1D()
+    # model.to(device)
+    # Settings = {'BP_optimizer': 'torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999), weight_decay=0)',        #  从零开始训练
+    #             'trainer': 'Model_Trainer(model,torch.nn.MSELoss(),BP_optimizer,device,Settings,batch_size=32,num_epochs=40,save_states=True,save_final=True)'}
+
+    Settings = {
+        'BP_optimizer': "torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-3, betas=(0.9, 0.999), weight_decay=0)",     # 加载预训练模型
+        'trainer': "Model_Trainer(model, torch.nn.MSELoss(), BP_optimizer, device, Settings, batch_size=32, num_epochs=50, save_states=True, save_final=True)"}
+    
+    # 加载预训练模型
+    pretrained_model_path = 'PTH/132036/trained_model.pth'
+    print(f"Loading pretrained model from {pretrained_model_path}")
+    model = torch.load(pretrained_model_path, map_location=device)
     model.to(device)
-    # 实例化优化器和模型训练器
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    layers_to_unfreeze = ["final_fc"]  # 综合最好的结果,"resnet.layer4", "resnet.layer3"
+    for name, param in model.named_parameters():
+        if any(layer in name for layer in layers_to_unfreeze):
+            param.requires_grad = True
+
+    # 设置优化器和训练器
     BP_optimizer = eval(Settings['BP_optimizer'])
     model_trainer = eval(Settings['trainer'])
 
