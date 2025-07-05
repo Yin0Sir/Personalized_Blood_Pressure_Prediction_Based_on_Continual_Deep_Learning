db5a601d7326236b216fd988220ce9dd1c30326b
Ark Master <1241094178@qq.com>
2025-07-05 18:34:05 +0800

    1

--- Model_Training/Model_Training_cl.py
+++ Model_Training/Model_Training_cl.py
@@ -34,30 +34,50 @@ Train_File3 = 'D:/Data/PulseDB/Supplementary_Subset_Files/Data_Task3.mat'
 Train_File4 = 'D:/Data/PulseDB/Supplementary_Subset_Files/Data_Task4.mat'
 Test_File = 'D:/Data/PulseDB/Supplementary_Subset_Files/Data_Task5.mat'
 
-Train_Data1 = Build_Dataset(Train_File1, 'DBP')
-Train_Data2 = Build_Dataset(Train_File2, 'DBP')
-Train_Data3 = Build_Dataset(Train_File3, 'DBP')
-Train_Data4 = Build_Dataset(Train_File4, 'DBP')
-Test_Data = Build_Dataset(Test_File, 'DBP')
+Train_Data1 = Build_Dataset(Train_File1, 'SBP')
+Train_Data2 = Build_Dataset(Train_File2, 'SBP')
+Train_Data3 = Build_Dataset(Train_File3, 'SBP')
+Train_Data4 = Build_Dataset(Train_File4, 'SBP')
+Test_Data = Build_Dataset(Test_File, 'SBP')
 
 if __name__ == '__main__':
+    ## 直接训练
+    # Seed(6) 
+    # model = CF_Basic_s.Resnet34_1D()
+    # Settings = {'BP_optimizer': 'torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999), weight_decay=0)',
+    #             'trainer': 'Model_Trainer(model,torch.nn.MSELoss(),BP_optimizer,device,Settings,batch_size=32,num_epochs=40,save_states=True,save_final=True)'}
+
+    # torch.cuda.empty_cache()
+    # device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
+    # print(torch.cuda.get_device_name(0))
+    # model.to(device)
+    # # 实例化优化器和模型训练器
+    # BP_optimizer = eval(Settings['BP_optimizer'])
+    # model_trainer = eval(Settings['trainer'])
+
+    # # 构造阶段数据列表
+    # stage_trainsets = [Train_Data1, Train_Data2, Train_Data3, Train_Data4]
+    # stage_testsets = [
+    # {"Test_Data": Test_Data},
+    # {"Test_Data": Test_Data},
+    # {"Test_Data": Test_Data},
+    # {"Test_Data": Test_Data}]
+
+    # # 调用多阶段训练方法
+    # model_trainer.Train_Model_MultiStage(stage_trainsets, stage_testsets)
+
+    ## 继续训练
     Seed(6)
     torch.cuda.empty_cache()
     device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu")
     print(torch.cuda.get_device_name(0))
 
-    # # 定义模型
-    # model = CF_Basic_s.Resnet34_1D()
-    # model.to(device)
-    # Settings = {'BP_optimizer': 'torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999), weight_decay=0)',        #  从零开始训练
-    #             'trainer': 'Model_Trainer(model,torch.nn.MSELoss(),BP_optimizer,device,Settings,batch_size=32,num_epochs=40,save_states=True,save_final=True)'}
-
     Settings = {
-        'BP_optimizer': "torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-3, betas=(0.9, 0.999), weight_decay=0)",     # 加载预训练模型
-        'trainer': "Model_Trainer(model, torch.nn.MSELoss(), BP_optimizer, device, Settings, batch_size=32, num_epochs=50, save_states=True, save_final=True)"}
+        'BP_optimizer': "torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4, betas=(0.9, 0.999), weight_decay=0)",     # 加载预训练模型
+        'trainer': "Model_Trainer(model, torch.nn.MSELoss(), BP_optimizer, device, Settings, batch_size=32, num_epochs=10, save_states=True, save_final=True)"}
     
     # 加载预训练模型
-    pretrained_model_path = 'PTH/132036/trained_model.pth'
+    pretrained_model_path = 'PTH/052005/trained_model.pth'    # 052005:EMBC-SBP;132036:EMBC-DBP
     print(f"Loading pretrained model from {pretrained_model_path}")
     model = torch.load(pretrained_model_path, map_location=device)
     model.to(device)
@@ -65,7 +85,7 @@ if __name__ == '__main__':
     for param in model.parameters():
         param.requires_grad = False
 
-    layers_to_unfreeze = ["final_fc"]  # 综合最好的结果,"resnet.layer4", "resnet.layer3"
+    layers_to_unfreeze = ["fc","layer4","layer4_cf","layer3","layer3_cf"]  # 综合最好的结果,"resnet.layer4", "resnet.layer3"
     for name, param in model.named_parameters():
         if any(layer in name for layer in layers_to_unfreeze):
             param.requires_grad = True
